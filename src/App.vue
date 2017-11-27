6c5ce51e02a5358af8889172fc7b2726de7077b7
markhorvath <markhorvath7@gmail.com>
2017-11-27 16:10:32 -0500

    init commit, started home page

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-app id="inspire">
+    <v-toolbar dark app class="primary" fixed>
+      <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up">
+      </v-toolbar-side-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer;">CRFC</router-link></v-toolbar-title>
+      <img src="../src/images/crfc-logo-light.png" style="height: 54px; margin-left: 8px;">
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-menu open-on-hover offset-y>
+          <v-btn flat slot="activator"><v-icon left>{{ icons[0] }}</v-icon>About</v-btn>
+            <v-list>
+              <v-list-tile v-for="item in aboutItems" :key="item.title" :to="item.link" @click="">
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+        </v-menu>
+        <v-btn
+          flat
+          :key="menuItems[0].title"
+          :to="menuItems[0].link">
+          <v-icon left>{{ menuItems[0].icon }}</v-icon>
+          {{ menuItems[0].title }}
+        </v-btn>
+        <v-menu open-on-hover offset-y>
+          <v-btn flat slot="activator"><v-icon left>{{ icons[1] }}</v-icon>News and Events</v-btn>
+            <v-list>
+              <v-list-tile v-for="item in newsItems" :key="item.title" :to="item.link" @click="">
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+        </v-menu>
+        <v-btn
+          flat
+          :key="menuItems[1].title"
+          :to="menuItems[1].link">
+          <v-icon left>{{ menuItems[1].icon }}</v-icon>
+          {{ menuItems[1].title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false,
+        menuItems: [
+          {icon: 'gavel', title: 'Build A Team', link: '/buildteam'},
+          {icon: 'email', title: 'Contact Us', link: '/contact'}
+        ],
+        icons: ['email', 'cast', 'gavel'],
+        aboutItems: [
+          { title: 'History', link: '/history' },
+          { title: 'Rules', link: '/rules' },
+          { title: 'Scoring', link: '/scoring' },
+          { title: 'Teams', link: '/teams' },
+          { title: 'Sponsors', link: '/sponsors' }
+        ],
+        newsItems: [
+          { title: 'Gallery', link: '/gallery' },
+          { title: 'Videos', link: '/videos' },
+          { title: 'Combine', link: '/combine' },
+          { title: 'Playoffs', link: '/playoffs' },
+          { title: 'Championship Records', link: '/records' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+.test {
+  background-color: #334d5c;
+}
+
+/*
+#inspire {
+  height: 100vh;
+}*/
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,128 @@
+<template>
+    <v-container>
+        <v-layout row wrap class="mt-0">
+            <v-flex xs12>
+                <v-carousel style="cursor: pointer;">
+                    <v-carousel-item class="carousel-item"
+                    v-for="meetup in meetups"
+                    :src="meetup.imgUrl"
+                    :key="meetup.id"
+                    @click="">
+                    <div class="title">
+                      {{ meetup.title }}
+                    </div>
+                    </v-carousel-item>
+                </v-carousel>
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap class="mt-3">
+            <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
+              <v-card>
+                <v-card-media
+                  class="white--text"
+                  height="400px"
+                  src="../src/images/build-team-lg.JPG"
+                >
+                <v-container fill-height fluid>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox style="margin: 0 auto;">
+                      <h2 wrap elevation-20 style="font-size: 40px !important; color: yellow !important;" class="headline">Build A Team</h2>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-media>
+              <v-card-title style="position: -100;">
+                <div>
+                  <span class="primary--text text-xs-center">Register by January 18, 2018</span>
+                </div>
+              </v-card-title>
+              <v-card-actions>
+                <v-btn secondary style="margin: 0 auto !important;">Learn More</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row wrap class="mt-3">
+            <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
+                <v-card primary>
+                    <v-card-title>
+                        <h1 class="text--white" style="margin: 0 auto;">Build a Team</h1>
+                    </v-card-title>
+                    <v-card-media
+                        src="../src/images/build-team-lg.JPG"
+                        height="450px"
+                    ></v-card-media>
+                    <v-card-text secondary>
+                        <div class="info--text">Register by Jan 1, 2018</div>
+                        <div>{{ teamCaption }}</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn secondary>Learn More</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+
+        <v-layout row wrap class="mt-2">
+        <v-flex xs12 sm6 id="sponsors">
+          <div class="text-xs-center">
+            <h1>Sponsors</h1>
+            <ul v-for="sponsor in sponsors">
+              <li> {{ sponsor }}</li>
+            </ul>
+          </div>
+        </v-flex>
+        <v-flex xs12 sm6 id="members">
+          <div class="text-xs-center">
+            <h1>Members</h1>
+            <ul v-for="member in members">
+              <li> {{ member }}</li>
+            </ul>
+          </div>
+        </v-flex>
+        </v-layout>
+        <div class="red" style="height: 200px; width: 1000px; margin: 0 auto;">
+        </div>
+    </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        meetups: [
+          {imgUrl: '../src/images/crfc-1-1000w.jpg', id: 'adkjadk123', title: 'RF1'},
+          {imgUrl: '../src/images/crfc-2-1000w.jpg', id: 'adkjadk123', title: 'RF2'},
+          {imgUrl: '../src/images/crfc-4-1000w.jpg', id: 'adkjadk123', title: 'RF4'},
+          {imgUrl: '../src/images/trophy-563h.jpg', id: 'adkjadk124', title: 'Trophy'}
+        ],
+        sponsors: ['GE', 'Visual Edge', 'Boeing', 'Yaskawa'],
+        members: ['University of Notre Dame', 'Ohio Northern University', 'U.S. Naval Academy', 'Valparaiso University', 'Purdue Polytechnic', 'University of Mount Union', 'Indian Universiy - Purdue University Indianapolis'],
+        teamCaption: 'This is our build a team page.  Blaldhalk  ;alkdak lkasdj;'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#build-team {
+  border: 4px solid #334d5c;
+}
+
+.carousel-item {
+  background-size: contain;
+}
+
+#sponsors h1, #members h1 {
+  color: #1e5d88;
+  font-size: 32px;
+}
+
+#sponsors ul, #members ul {
+  list-style-type: none;
+  font-size: 20px;
+}
+</style>
