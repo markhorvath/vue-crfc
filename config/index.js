bb53ffdb2e37be29be5457d2fef4b81bc0859331
markhorvath <markhorvath7@gmail.com>
2017-11-28 14:52:50 -0500

    changed assetspublicpath in index.js for images

--- config/index.js
+++ config/index.js
@@ -55,7 +55,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    // I took out the '/' below which enabled my .png logo to show on gh-pages
+    assetsPublicPath: './',
 
     /**
      * Source Maps
